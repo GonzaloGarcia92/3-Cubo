78129aab2869c4c470bc381826825565aba57609
Agustin Burset <agustin.burset@icloud.com>
2020-03-31 10:47:13 -0300

    Draw cube

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { getCanvasElement, getWebGL2Context, createShader, createProgram, createVertexBuffer, bindAttributeToVertexBuffer, createIndexBuffer } from "./utils/gl-utils.js"
+import { getCanvasElement, getWebGL2Context, createShader, createProgram, createVertexBuffer, bindAttributeToVertexBuffer, createIndexBuffer, magic } from "./utils/gl-utils.js"
 import { vertexShaderSourceCode, fragmentShaderSourceCode } from "./utils/shaders.js"
 
 // #️⃣ Configuración base de WebGL
@@ -20,30 +20,56 @@ const program = createProgram(gl, vertexShader, fragmentShader)
 const vertexPositionLocation = gl.getAttribLocation(program, 'vertexPosition')
 const vertexColorLocation = gl.getAttribLocation(program, 'vertexColor')
 
-// #️⃣ Definimos la info de la geometría que vamos a dibujar (un cuadrado)
+// #️⃣ Definimos la info de la geometría que vamos a dibujar (un cubo)
 
 const vertexPositions = [
-  -0.5, -0.5, // 0 👈 indice de cada posición
-  0.5, -0.5,  // 1
-  0.5, 0.5,   // 2
-  -0.5, 0.5   // 3
+  -1, 1, 1,   // 0 👈 indice de cada posición
+  1, 1, 1,    // 1
+  1, 1, -1,   // 2
+  -1, 1, -1,  // 3
+  -1, -1, 1,  // 4
+  1, -1, 1,   // 5
+  1, -1, -1,  // 6
+  -1, -1, -1  // 7
 ]
 
 const vertexColors = [
-  1, 1, 1,    // 0 👈 indice de cada color
-  1, 0, 0,    // 1
-  0, 1, 0,    // 2
-  0, 0, 1     // 3
+  1, 0, 1,    // 0 👈 indice de cada color
+  1, 1, 1,    // 1
+  0, 1, 1,    // 2
+  0, 0, 1,    // 3
+  1, 0, 0,    // 4
+  1, 1, 0,    // 5
+  0, 1, 0,    // 6
+  0, 0, 0     // 7
 ]
 
 const indices = [
-  // primer triangulo
+  // cara de arriba
   0, 1, 3,
-  // segundo triangulo
-  3, 1, 2
+  3, 1, 2,
+  // abajo
+  7, 5, 4,
+  5, 7, 6,
+  // izquierda
+  3, 4, 0,
+  3, 7, 4,
+  // derecha
+  5, 2, 1,
+  5, 6, 2,
+  // adelante
+  4, 1, 0,
+  4, 5, 1,
+  // atrás
+  6, 3, 2,
+  6, 7, 3,
 ]
 
-// #️⃣ Guardamos la info del cuadrado (i.e. posiciones, colores e indices) en VBOs e IBOs
+/* 📝 Cada cara esta formada por dos triángulos, donde sus indices siguen la convención de sentido
+ * anti-horario 🔄
+ */
+
+// #️⃣ Guardamos la info de la geometría en VBOs e IBOs
 
 const vertexPositionsBuffer = createVertexBuffer(gl, vertexPositions)
 const vertexColorsBuffer = createVertexBuffer(gl, vertexColors)
@@ -59,7 +85,7 @@ gl.bindVertexArray(vertexArray)
 
 // Habilitamos cada atributo y lo conectamos a su buffer
 gl.enableVertexAttribArray(vertexPositionLocation)
-bindAttributeToVertexBuffer(gl, vertexPositionLocation, 2, vertexPositionsBuffer)
+bindAttributeToVertexBuffer(gl, vertexPositionLocation, 3, vertexPositionsBuffer)
 gl.enableVertexAttribArray(vertexColorLocation)
 bindAttributeToVertexBuffer(gl, vertexColorLocation, 3, vertexColorsBuffer)
 
@@ -74,7 +100,10 @@ gl.bindVertexArray(null)
 gl.useProgram(program)
 gl.bindVertexArray(vertexArray)
 
-// #️⃣ Dibujamos la escena (nuestro majestuoso cuadrado)
+// ✨ Magia (que mas adelante vamos a ver qué esta haciendo)
+magic(gl, program, canvas)
+
+// #️⃣ Dibujamos la escena
 
 // Limpiamos el canvas
 gl.clear(gl.COLOR_BUFFER_BIT)
